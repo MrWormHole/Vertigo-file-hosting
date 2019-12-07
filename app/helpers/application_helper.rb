a6b440f35a46aed256d2fc0637aff6ab9543fc8b
Talha Altınel <talhaaltinel@hotmail.com>
2019-12-07 20:49:38 +0300

    styling errors and other forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def show_notice
+        if !flash[:notice].blank?
+            return flash[:notice]
+        end
+    end
+
+    def show_error
+        if !flash[:error].blank?
+            return flash[:error]
+        end
+    end
 end
