c5d618eec07f75253982ed248bfa34a164b98af6
Talha Altınel <talhaaltinel@hotmail.com>
2019-12-07 23:36:45 +0300

    bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable, :lockable, :timeoutable
-  has_many :attachments
+  has_many :attachments, :dependent => :destroy
 end
