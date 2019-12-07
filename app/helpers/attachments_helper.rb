e4b4459ea75621be434e82077164e6a5ba90d52a
Talha Altınel <talhaaltinel@hotmail.com>
2019-12-07 14:53:10 +0300

    attachment helper is added

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,2 +1,15 @@
 module AttachmentsHelper
+    def get_file_size(attachment = @attachment)
+        file_size = attachment.file.blob.byte_size
+        
+        if file_size < 1000
+          return "#{file_size} KB"
+        elsif file_size < 1000 ** 2
+          return "#{(file_size/1000.0 * 10).round / 10.0} MB"
+        elsif file_size < 1000 ** 3
+          return "#{((file_size/1000.0 ** 2) * 10).round / 10.0} GB"
+        end
+    
+        return "file size couldn't be determined"
+      end
 end
