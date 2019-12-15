283430f9cdf4a7303a3855b82cb96dc351a76378
MrWormHole <talhaaltinel@hotmail.com>
2019-12-15 16:26:52 +0300

    fixing build error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
-  :user_name => <%= ENV['SENDGRID_USERNAME'] %>,
-  :password => <%= ENV['SENDGRID_PASSWORD'] %>,
+  user_name <%= ENV['SENDGRID_USERNAME'] %>,
+  password: <%= ENV['SENDGRID_PASSWORD'] %>,
   :domain => 'https://vertigo-files.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 465,
