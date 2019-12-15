17588b578964bbd936c9863b320d3d30d341201c
Talha Altınel <talhaaltinel@hotmail.com>
2019-12-15 17:52:33 +0300

    bug fix for user file usage

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,6 +1,5 @@
 class AttachmentsController < ApplicationController
   before_action :authenticate_user!,:except => [:show]
-  # before_action :prevent_user_forgery, :only => [:edit,:update,:delete,:destroy]
   
   def index
     @attachments = current_user.attachments
@@ -8,15 +7,14 @@ class AttachmentsController < ApplicationController
   end
 
   def show
-    if @attachment.visible 
-      @attachment = Attachment.find(params[:id])
-      redirect_to attachment_path(@attachment)
+    @attachment = Attachment.find(params[:id])
+    if !@attachment.visible 
+      redirect_to attachments_path
       # which will use the primary ‘id’ key or your ‘hash_id’ to look up records. There you have it!
     end
     
     # this is gonna be for showing a download link
     # get it and show it if user allowed it to be seen only
-    # @attachment = Attachment.find(params[:id])
   end
 
   def new
@@ -27,7 +25,7 @@ class AttachmentsController < ApplicationController
     @attachment = current_user.attachments.build(attachment_params)
 
     if @attachment.save
-      file_load = 0 
+      file_load = current_user.used_file_size_in_gb
       file_load += @attachment.file.blob.byte_size / 10.0 ** 9
       current_user.update(:used_file_size_in_gb => file_load)
       flash[:notice] = "Success! File created"
@@ -77,22 +75,6 @@ class AttachmentsController < ApplicationController
   private
   def attachment_params
     params.require(:attachment).permit(:name, :file)
-  end
-
-  def prevent_user_forgery
-    @attachment = Attachment.find(params[:id])
-    if @attachment.user_id != current_user.id
-      @authority_provided = false
-      redirect_to :controller => 'application', :action => 'home'
-    else
-      @authority_provided = true
-    end
-  end
-
-  def inc_download_count
-    @attachment = Attachment.find(params[:id])
-    @attachment.download_count += 1
-    @attachment.save
   end
 
 end
