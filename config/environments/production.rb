e606d3fdd7cd29415c8f39540953a32b85c21763
MrWormHole <talhaaltinel@hotmail.com>
2019-12-15 16:27:26 +0300

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
-  user_name <%= ENV['SENDGRID_USERNAME'] %>,
+  user_name: <%= ENV['SENDGRID_USERNAME'] %>,
   password: <%= ENV['SENDGRID_PASSWORD'] %>,
   :domain => 'https://vertigo-files.herokuapp.com',
   :address => 'smtp.sendgrid.net',
